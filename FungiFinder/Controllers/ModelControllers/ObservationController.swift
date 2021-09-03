3cdec39c01aac148389cc5be3c7d13fb245aa484
Kyle Warren <basedinbham@gmail.com>
2021-09-03 09:24:01 -0700

    delete added

--- FungiFinder/Controllers/ModelControllers/ObservationController.swift
+++ FungiFinder/Controllers/ModelControllers/ObservationController.swift
@@ -11,7 +11,7 @@ class ObservationController {
     
     static let shared = ObservationController()
     var observations: [Observation] = []
-    let publicDB = CKContainer.default().publicCloudDatabase
+    let privateDB = CKContainer.default().privateCloudDatabase
     
     private lazy var fetchRequest: NSFetchRequest<Observation> = {
         let request = NSFetchRequest<Observation>(entityName: "Observation")
@@ -24,18 +24,36 @@ class ObservationController {
     func createObservation(with name: String, date: Date, notes: String, reminder: Date, type: String, latitude: String, longitude: String) {
         let observation = Observation(date: date, image: nil, latitude: latitude, longitude: longitude, name: name, notes: notes, reminder: reminder, type: type)
         observations.append(observation)
+        CoreDataStack.saveContext()
     }
     
     func fetchOBservations() {
         let observations = (try? CoreDataStack.context.fetch(fetchRequest)) ?? []
         self.observations = observations
-        CoreDataStack.saveContext()
 
     }
     
+    func updateObservation(_ observation: Observation, name: String, date: Date, notes: String, reminder: Date, type: String, latitude: String, longitude: String) {
+        observation.name = name
+        observation.date = date
+        observation.notes = notes
+        observation.reminder = reminder
+        observation.type = type
+        observation.latitude = latitude
+        observation.longitude = longitude
+        
+        CoreDataStack.saveContext()
+    }
     
-    
-    
+    func deleteObservation(observation: Observation) {
+        if let index = observations.firstIndex(of: observation) {
+            observations.remove(at: index)
+            CoreDataStack.context.delete(observation)
+            CoreDataStack.saveContext()
+            
+            // clear notifications for observation
+        }
+    }
     
     
     
--- FungiFinder/CoreData/NSManagedObjectContext.swift
+++ FungiFinder/CoreData/NSManagedObjectContext.swift
@@ -4,29 +4,29 @@
 //
 //  Created by Kyle Warren on 9/2/21.
 //
-
-import CoreData
-
-extension NSManagedObjectContext {
-    func saveContext() {
-        do {
-            try save()
-        } catch {
-            let nserror = error as NSError
-            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-        }
-    }
-    
-    func delete(_ observations: [Observation]) {
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Observation")
-        request.predicate = NSPredicate(format: "id IN %@", observations.map { $0.id?.uuidString })
-        do {
-        let results = (try fetch(request) as? [Observation]) ?? []
-            results.forEach { delete($0) }
-        } catch {
-            print("Failied deleting provided objects")
-            return
-        }
-        saveContext()
-    }
-}
+//
+//import CoreData
+//
+//extension NSManagedObjectContext {
+//    func saveContext() {
+//        do {
+//            try save()
+//        } catch {
+//            let nserror = error as NSError
+//            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+//        }
+//    }
+//
+//    func delete(_ observations: [Observation]) {
+//        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Observation")
+//        request.predicate = NSPredicate(format: "id IN %@", observations.map { $0.id?.uuidString })
+//        do {
+//        let results = (try fetch(request) as? [Observation]) ?? []
+//            results.forEach { delete($0) }
+//        } catch {
+//            print("Failied deleting provided objects")
+//            return
+//        }
+//        saveContext()
+//    }
+//}
